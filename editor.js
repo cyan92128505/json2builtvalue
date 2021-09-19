b839def3af38aa7d20fdcc4b5c2a2d30d49d211f
cyan92128505 <cyan92128505@gmail.com>
2021-09-19 08:30:12 +0800

    Modify UI

--- editor.js
+++ editor.js
@@ -1,6 +1,11 @@
-var inputCodeMirror = CodeMirror(input_text, {
-  mode: "dart",
-});
-var outputCodeMirror = CodeMirror(output_text, {
-  mode: "dart",
-});
+var _option = {
+  lineNumbers: true,
+  mode: "application/dart",
+  theme: "monokai",
+};
+
+var inputCodeMirror = CodeMirror(input_text, _option);
+var outputCodeMirror = CodeMirror(output_text, _option);
+
+inputCodeMirror.setSize("48%", "90vh");
+outputCodeMirror.setSize("48%", "90vh");
